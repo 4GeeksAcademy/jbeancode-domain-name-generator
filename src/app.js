dc8d3d6992301db03423e56ea3ab22fe20a1a974
JBeanCode <js02bean@gmail.com>
2026-01-14 16:00:30 +0000

    joshs-first draft-domainNameGenerator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,31 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+
+let pronouns = ['The', 'Our', 'Thier', 'Your', 'His'];
+let adjectives = ['chonky', 'hairy', 'lovable', 'annoying', 'desperate'];
+let nouns = ['cat', 'aunt', 'sasquatch', 'Yoshi', 'BillMurray'];
+
+// for (let pronoun in pronouns) {
+//   for(let adjective in adjectives){
+//     for(let noun in nouns){
+//       console.log(`${pronouns[pronoun]}${adjectives[adjective]}${nouns[noun]}.com`)
+//     }
+//   }
+// }
+
+const domainNameSum = function (arr){
+    let randInt = (Math.floor(Math.random()* 5))
+    return arr[randInt]
+}
+      // console.log(domainNameSum(pronouns))
+      // console.log(domainNameSum(adjectives))
+      // console.log(domainNameSum(nouns))
+      let newDomainName = console.log(`www.${domainNameSum(pronouns)}${domainNameSum(adjectives)}${domainNameSum(nouns)}.com`)
+    };
+const button = document.getElementById("myButton");
+function changeText() {
+  paragraph.innerHTML = `www.${domainNameSum(pronouns)}${domainNameSum(adjectives)}${domainNameSum(nouns)}.com`;
+}
+document.getElementById("paragraph");
+button.addEventListener("click", changeText);
